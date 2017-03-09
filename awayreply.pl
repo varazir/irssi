683ad91a4840bc5c07996c972b9dcc9e33b85441
varazir <varazir@gmail.com>
2017-03-09 12:31:11 +0100

    First blod

---
+++ awayreply.pl
@@ -0,0 +1,13 @@
+use strict;
+use Irssi;
+use vars qw($VERSION %IRSSI);
+
+$VERSION="0.0.1";
+%IRSSI = (
+	authors=> 'varazir',
+	contact=> 'varazir@freenode',
+	name=> 'awayreply',
+	description=> 'Reply to a new private message if you are away',
+	license=> 'GPL v2',
+	url=> '',
+);
